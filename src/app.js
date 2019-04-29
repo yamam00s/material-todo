f9795ea354eec7a949d4a9113023ef88a516aa67
yamam00s <s.yamamo0327@gmail.com>
2019-04-30 00:00:14 +0900

    直接DOM操作でAddTodo

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
-import ItemView from './views/ItemView';
+import { createElement } from './utils/createTodoElement';
 
-const itemView = new ItemView();
-itemView.mounted();
+export default class App {
+  // eslint-disable-next-line class-methods-use-this
+  mounted() {
+    const formElement = document.querySelector('#js-form');
+    const inputElement = document.querySelector('#js-form-input');
+    const containerElement = document.querySelector('#js-todo-list');
+    const todoItemCountElement = document.querySelector('#js-todo-count');
+
+    // Todoアイテム数
+    let todoItemCount = 0;
+
+    formElement.addEventListener('submit', event => {
+      // 本来のsubmitイベントの動作を止める
+      event.preventDefault();
+      // 追加するTodoアイテムの要素(li要素)を作成する
+      const todoItemElement = createElement`<li>${inputElement.value}<li>`;
+      // Todoアイテムをcontainerに追加する
+      containerElement.appendChild(todoItemElement);
+      // Todoアイテム数を+1し、表示されてるテキストを更新する
+      todoItemCount += 1;
+      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      // 入力欄を空文字にしてリセットする
+      inputElement.value = '';
+    });
+  }
+}
---
+++ src/utils/createTodoElement.js
@@ -0,0 +1,49 @@
+const helper = {
+  escapeSpecialChars(str) {
+    return str
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#039;');
+  },
+
+  htmlToTemplateElement(htmlString) {
+    const template = document.createElement('template');
+    template.innerHTML = htmlString;
+    return template.content.firstElementChild;
+  }
+};
+
+/**
+ * HTML文字列からDOM Nodeを作成して返すタグ関数
+ *
+ * @param {Array} Strings
+ * @param {RestParameters} values
+ * @return {Element}
+ */
+export function createElement(strings, ...values) {
+  const htmlString = strings.reduce((prev, current, index) => {
+    const value = values[index - 1];
+    // 文字列の場合のみエスケープ
+    if (typeof value === 'string') {
+      return prev + helper.escapeSpecialChars(value);
+    }
+    return prev + String(value) + current;
+  });
+  return helper.htmlToTemplateElement(htmlString);
+}
+
+/**
+ * コンテナ要素の中身をbodyElementで上書きする
+ *
+ * @param {Element} bodyElement コンテナ要素の中身となる要素
+ * @param {Element} containerElement コンテナ要素
+ */
+export function render(bodyElement, containerElement) {
+  // rootElementの中身を空にする
+  // eslint-disable-next-line no-param-reassign
+  containerElement.innerHTML = '';
+  // rootElementの直下にbodyElementを追加する
+  containerElement.appendChild(bodyElement);
+}
